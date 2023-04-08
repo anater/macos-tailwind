a2757b807f7ff3e72569da302333c42223fef0ef
Andrew Nater <a.a.nater@gmail.com>
2023-04-08 15:20:41 -0400

    Add HelpButton

---
+++ stories/HelpButton.stories.js
@@ -0,0 +1,43 @@
+import "../site/styles.src.css"
+import HelpButtonIcon from "./assets/HelpButtonIcon.svg"
+
+export default {
+  title: "Controls/Help Button",
+  tags: ["autodocs"],
+  render: ({ disabled }) => {
+    const disabledAttr = disabled ? "disabled" : ""
+    return `
+    <button ${disabledAttr}
+      class="
+        rounded-full
+        w-5
+        h-5
+        border-0.5
+        border-black/5
+        shadow-sm
+        flex
+        items-center
+        justify-center
+        active:bg-gray-100
+        disabled:pointer-events-none
+        disabled:opacity-40"
+    >
+      <img src="${HelpButtonIcon}" class="object-contain" />
+    </button>`
+  },
+  argTypes: {
+    disabled: false,
+  },
+}
+
+export const Default = {
+  args: {
+    disabled: false,
+  },
+}
+
+export const Disabled = {
+  args: {
+    disabled: true,
+  },
+}
