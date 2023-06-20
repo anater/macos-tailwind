88bc9ca229278cb437cfbcd7b03b6a7466c76048
Andrew Nater <a.a.nater@gmail.com>
2023-06-20 12:45:57 -0400

    Update dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,18 @@
 /** @type { import('@storybook/html-webpack5').StorybookConfig } */
 const config = {
   stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    {
-      name: "@storybook/addon-styling",
-      options: { postCss: true },
-    },
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", {
+    name: "@storybook/addon-styling",
+    options: {
+      postCss: true
+    }
+  }, "@storybook/addon-mdx-gfm"],
   framework: {
     name: "@storybook/html-webpack5",
-    options: {},
+    options: {}
   },
   docs: {
-    autodocs: "tag",
-  },
+    autodocs: "tag"
+  }
 };
 export default config;
