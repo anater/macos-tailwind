a7a598085dfa9ebe74d7430e8ce5e96c9cdf0817
Andrew Nater <a.a.nater@gmail.com>
2023-07-25 15:01:36 -0400

    Finish small list 1

--- stories/SmallList.stories.js
+++ stories/SmallList.stories.js
@@ -1,6 +1,7 @@
 import PlusIcon from "./assets/LargeListPlusIcon.svg"
 import MinusIcon from "./assets/LargeListMinusIcon.svg"
 import SettingsIcon from "./assets/LargeListSettingsIcon.svg"
+import { map } from "./helpers"
 
 const ICONS = [
   ["Add", PlusIcon],
@@ -15,25 +16,26 @@ export default {
     return `
     <div class="w-[170px] flex flex-col bg-white border border-fills-opaque-4">
       <ul class="h-[182px] overflow-y-scroll bg-white text-base text-text-opaque-light-1 font-light">
-      ${items
-        .map(
-          (text) =>
-            /*html*/ `<li class="py-1 px-[6px] outline-none focus:bg-default-light focus:text-white" tabindex=0>${text}</li>`
-        )
-        .join("\n")}
+      ${map(
+        items,
+        (item) =>
+          /*html*/ `<li class="py-1 px-[6px] outline-none focus:bg-default-light focus:text-white" tabindex=0>${item}</li>`
+      )}
       </ul>
 
       <footer class="h-6 border-t border-fills-opaque-3 flex items-stretch">
-      ${ICONS.map(
+      ${map(
+        ICONS,
         ([label, path], index) => /*html*/ `
-      <button class="px-2"><img src="${path}" alt="${label}" /></button>
-      ${
-        index < ICONS.length - 1
-          ? `<div class="w-[1px] h-4 self-center bg-fills-opaque-4" aria-hidden></div> <!-- separator -->`
-          : ``
-      }
-      `
-      ).join("\n")}
+          <button class="px-2">
+            <img src="${path}" alt="${label}" title="${label}"/>
+          </button>
+          ${
+            index < ICONS.length - 1
+              ? `<div class="w-[1px] h-4 self-center bg-fills-opaque-4" aria-hidden></div> <!-- separator -->`
+              : ``
+          }`
+      )}
       </footer>
     </div>`
   },
